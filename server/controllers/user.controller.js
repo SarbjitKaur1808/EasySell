b9c456de87037a79b60c758eb01f5534d00466c5
Vivek Kundaliya <vkundali@my.centennialcollege.ca>
2023-11-10 20:49:06 -0500

    Added method to get user by ID

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -15,4 +15,20 @@ const signup = async (req, res) => {
     }
 };
 
-export default { signup };
+const getUserById = async (req, res, next, id) => {
+  try {
+    let user = await User.findById(id);
+    if (!user)
+      return res.status("400").json({
+        error: "User not found",
+      });
+    req.profile = user;
+    next();
+  } catch (err) {
+    return res.status("400").json({
+      error: "Could not retrieve user",
+    });
+  }
+};
+
+export default { signup, getUserById };
