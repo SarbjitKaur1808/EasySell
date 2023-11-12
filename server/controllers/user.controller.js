2527363cfcfab866daf046ae3468fdbd3025b111
GithubDamini <dbhoi2@my.centennialcollege.ca>
2023-11-12 14:44:56 -0500

    Fixed minor error

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -77,7 +77,7 @@ const read = (req, res) => {
   req.profile.hashed_password = undefined;
   req.profile.salt = undefined;
   return res.json(req.profile);
-
+};
 // Lists all users
 const list = async (req, res) => {
   try {
