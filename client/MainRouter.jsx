a8716636198e334e01c7c382cf74e8b514d8ac84
Sarbjit <sarbjitkaurjld@gmail.com>
2023-11-29 22:46:25 -0500

    added menu bar

--- client/MainRouter.jsx
+++ client/MainRouter.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import {Route, Switch} from 'react-router-dom'
-import Home from './core/Home'
-import Signup from './user/Signup'
-import Signin from './auth/Signin'
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./core/Home";
+import Signup from "./user/Signup";
+import Signin from "./auth/Signin";
+import Menu from "./core/Menu";
 
 const MainRouter = () => {
-  return (<div>
-     
+  return (
+    <div>
+      <Menu />
       <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route path="/signup" component={Signup}/>
-        <Route path="/signin" component={Signin}/>
+        <Route exact path="/" component={Home} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/signin" component={Signin} />
       </Switch>
-    </div>)
-}
+    </div>
+  );
+};
 
-export default MainRouter
+export default MainRouter;
