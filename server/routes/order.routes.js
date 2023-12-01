79a0b5196488ac0f65bc0eca0d72a339c25a1d1e
AniyaCentennial <a1203@my.centennialcollege.ca>
2023-12-01 01:05:26 -0500

    route

--- server/routes/order.routes.js
+++ server/routes/order.routes.js
@@ -7,31 +7,31 @@ import userCtrl from '../controllers/user.controller.js'
 
 const router = express.Router()
 
-router.route('/orders/:userId')
-    .post(authCtrl.requireSignin, userCtrl.stripeCustomer, productCtrl.decreaseQuantity, orderCtrl.create)
+router.route('/api/orders/:userId')
+  .post(authCtrl.requireSignin, userCtrl.stripeCustomer, productCtrl.decreaseQuantity, orderCtrl.create)
 
-router.route('/orders/shop/:shopId')
-    .get(authCtrl.requireSignin, shopCtrl.isOwner, orderCtrl.listByShop)
+router.route('/api/orders/shop/:shopId')
+  .get(authCtrl.requireSignin, shopCtrl.isOwner, orderCtrl.listByShop)
 
-router.route('/orders/user/:userId')
-    .get(authCtrl.requireSignin, orderCtrl.listByUser)
+router.route('/api/orders/user/:userId')
+  .get(authCtrl.requireSignin, orderCtrl.listByUser)
 
-router.route('/order/status_values')
-    .get(orderCtrl.getStatusValues)
+router.route('/api/order/status_values')
+  .get(orderCtrl.getStatusValues)
 
-router.route('/order/:shopId/cancel/:productId')
-    .put(authCtrl.requireSignin, shopCtrl.isOwner, productCtrl.increaseQuantity, orderCtrl.update)
+router.route('/api/order/:shopId/cancel/:productId')
+  .put(authCtrl.requireSignin, shopCtrl.isOwner, productCtrl.increaseQuantity, orderCtrl.update)
 
-router.route('/order/:orderId/charge/:userId/:shopId')
-    .put(authCtrl.requireSignin, shopCtrl.isOwner, userCtrl.createCharge, orderCtrl.update)
+router.route('/api/order/:orderId/charge/:userId/:shopId')
+  .put(authCtrl.requireSignin, shopCtrl.isOwner, userCtrl.createCharge, orderCtrl.update)
 
-router.route('/order/status/:shopId')
-    .put(authCtrl.requireSignin, shopCtrl.isOwner, orderCtrl.update)
+router.route('/api/order/status/:shopId')
+  .put(authCtrl.requireSignin, shopCtrl.isOwner, orderCtrl.update)
 
-router.route('/order/:orderId')
-    .get(orderCtrl.read)
+router.route('/api/order/:orderId')
+  .get(orderCtrl.read)
 
-router.param('userId', userCtrl.getUserById)
+router.param('userId', userCtrl.userByID)
 router.param('shopId', shopCtrl.shopByID)
 router.param('productId', productCtrl.productByID)
 router.param('orderId', orderCtrl.orderByID)
