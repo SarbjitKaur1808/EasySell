c168dad799fabb7b18950265240e0d59b54325de
Sarbjit <sarbjitkaurjld@gmail.com>
2023-12-04 09:50:48 -0500

    changes in home page

--- client/core/Home.jsx
+++ client/core/Home.jsx
@@ -1,106 +1,89 @@
 import React, { useState, useEffect } from "react";
-import { Card, CardContent, Typography, Grid, TextField, IconButton, Paper, InputBase } from "@material-ui/core";
-import SearchIcon from '@material-ui/icons/Search';
-import { makeStyles } from "@material-ui/core/styles";
+import { Card, CardContent, Typography, Grid } from "@material-ui/core";
 import { list } from "../product/api-product";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    padding: '2px 4px',
-    display: 'flex',
-    alignItems: 'center',
-    width: '100%',
-    maxWidth: 800, // adjust this to match the image
-    margin: 'auto',
-    marginBottom: theme.spacing(2),
-  },
-  input: {
-    marginLeft: theme.spacing(1),
-    flex: 1,
-  },
-  iconButton: {
-    padding: 10,
-  },
-  divider: {
-    height: 28,
-    margin: 4,
-  },
-}));
-
+import { ImageList, ImageListItem } from "@material-ui/core";
 export default function Home() {
-  const classes = useStyles();
   const [products, setProducts] = useState([]);
-  const [searchQuery, setSearchQuery] = useState('');
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
 
   useEffect(() => {
     setLoading(true);
-    list({ search: searchQuery }).then((data) => {
-      if (data.error) {
-        setError(data.error);
-      } else {
-        setProducts(data);
-      }
-      setLoading(false);
-    });
-  }, [searchQuery]);
+    const loadProducts = () => {
+      list().then((data) => {
+        if (data.error) {
+          setError(data.error);
+        } else {
+          setProducts(data);
+        }
+        setLoading(false);
+      });
+    };
 
-  const handleSearch = () => {
-    setLoading(true);
-    list({ search: searchQuery }).then((data) => {
-      if (data.error) {
-        setError(data.error);
-      } else {
-        setProducts(data);
-      }
-      setLoading(false);
-    });
+    loadProducts();
+  }, []);
+
+  const renderImage = (image) => {
+    if (typeof image === "string") {
+      return `data:${image.contentType};base64,${image.data}`;
+    }
   };
 
-  return (
-    <div>
-      <h1>Welcome</h1>
-      <Paper component="form" className={classes.root} elevation={1} square>
-        <InputBase
-          className={classes.input}
-          placeholder="Search products"
-          inputProps={{ 'aria-label': 'search products' }}
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-          onKeyPress={(e) => {
-            if (e.key === 'Enter') {
-              e.preventDefault();
-              handleSearch();
-            }
-          }}
-        />
-        <IconButton type="submit" className={classes.iconButton} aria-label="search" onClick={handleSearch}>
-          <SearchIcon />
-        </IconButton>
-      </Paper>
-      {/* Loading, error, and product grid code remains unchanged */}
-      {loading && <Typography>Loading products...</Typography>}
-      {error && <Typography color="error">{error}</Typography>}
-      {!loading && products.length > 0 && (
-        <Grid container spacing={3}>
-          {products.map((product) => (
-            <Grid item xs={12} sm={6} md={4} key={product._id}>
-              <Card style={{ margin: "10px" }}>
-                <CardContent>
-                  <Typography variant="h5">{product.name}</Typography>
-                  <Typography variant="body2">{product.description}</Typography>
-                  <img
-                    src={"/api/product/image/" + product._id}
-                    alt={product.name}
-                    style={{ maxWidth: "100%", height: "auto" }}
-                  />
-                </CardContent>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
-      )}
-    </div>
-  );
+//   return (
+//     <div>
+//       <h1>Welcome</h1>
+//       {loading && <Typography>Loading products...</Typography>}
+//       {error && <Typography color="error">{error}</Typography>}
+//       {!loading && products.length > 0 && (
+//         <Grid container spacing={3}>
+//           {products.map((product) => (
+//             <Grid item xs={12} sm={6} md={4} key={product._id}>
+//               <Card style={{ margin: "10px" }}>
+//                 <CardContent>
+//                   <Typography variant="h5">{product.name}</Typography>
+//                   <Typography variant="body2">{product.description}</Typography>
+//                   <img
+//   src={"/api/product/image/" + product._id}
+//   alt={product.name}
+//   style={{ height: "300px", maxWidth: "100%", objectFit: "contain" }}
+// />
+
+
+//                 </CardContent>
+//               </Card>
+//             </Grid>
+//           ))}
+//         </Grid>
+//       )}
+//     </div>
+//   );
+// }
+
+return (
+  <div>
+    <h1>Welcome</h1>
+    {loading && <Typography>Loading products...</Typography>}
+    {error && <Typography color="error">{error}</Typography>}
+    {!loading && products.length > 0 && (
+      <Grid container spacing={3}>
+        {products.map((product) => (
+          <Grid item xs={12} sm={6} md={3} lg={2} key={product._id}> {/* Adjusted for more items per row */}
+            <Card style={{ maxWidth: "300px", height: "100%" }}> {/* Adjusted Card Size */}
+              <CardContent>
+                <Typography variant="h5">{product.name}</Typography>
+                <Typography variant="body2">{product.description}</Typography>
+                <img
+                  src={"/api/product/image/" + product._id}
+                  alt={product.name}
+                  style={{ width: "100%", height: "200px", objectFit: "cover" }} 
+                />
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+        
+      </Grid>
+    )}
+  </div>
+);
 }
