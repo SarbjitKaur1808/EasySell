49007a5f447d0c7a1a2cb7a6147f41f64b28014a
GithubDamini <dbhoi2@my.centennialcollege.ca>
2023-12-04 00:32:58 -0500

    Search Products

--- client/core/Home.jsx
+++ client/core/Home.jsx
@@ -1,37 +1,85 @@
 import React, { useState, useEffect } from "react";
-import { Card, CardContent, Typography, Grid } from "@material-ui/core";
+import { Card, CardContent, Typography, Grid, TextField, IconButton, Paper, InputBase } from "@material-ui/core";
+import SearchIcon from '@material-ui/icons/Search';
+import { makeStyles } from "@material-ui/core/styles";
 import { list } from "../product/api-product";
-import { ImageList, ImageListItem } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    width: '100%',
+    maxWidth: 800, // adjust this to match the image
+    margin: 'auto',
+    marginBottom: theme.spacing(2),
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  },
+  divider: {
+    height: 28,
+    margin: 4,
+  },
+}));
+
 export default function Home() {
+  const classes = useStyles();
   const [products, setProducts] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
 
   useEffect(() => {
     setLoading(true);
-    const loadProducts = () => {
-      list().then((data) => {
-        if (data.error) {
-          setError(data.error);
-        } else {
-          setProducts(data);
-        }
-        setLoading(false);
-      });
-    };
+    list({ search: searchQuery }).then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProducts(data);
+      }
+      setLoading(false);
+    });
+  }, [searchQuery]);
 
-    loadProducts();
-  }, []);
-
-  const renderImage = (image) => {
-    if (typeof image === "string") {
-      return `data:${image.contentType};base64,${image.data}`;
-    }
+  const handleSearch = () => {
+    setLoading(true);
+    list({ search: searchQuery }).then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProducts(data);
+      }
+      setLoading(false);
+    });
   };
 
   return (
     <div>
       <h1>Welcome</h1>
+      <Paper component="form" className={classes.root} elevation={1} square>
+        <InputBase
+          className={classes.input}
+          placeholder="Search products"
+          inputProps={{ 'aria-label': 'search products' }}
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          onKeyPress={(e) => {
+            if (e.key === 'Enter') {
+              e.preventDefault();
+              handleSearch();
+            }
+          }}
+        />
+        <IconButton type="submit" className={classes.iconButton} aria-label="search" onClick={handleSearch}>
+          <SearchIcon />
+        </IconButton>
+      </Paper>
+      {/* Loading, error, and product grid code remains unchanged */}
       {loading && <Typography>Loading products...</Typography>}
       {error && <Typography color="error">{error}</Typography>}
       {!loading && products.length > 0 && (
@@ -42,13 +90,11 @@ export default function Home() {
                 <CardContent>
                   <Typography variant="h5">{product.name}</Typography>
                   <Typography variant="body2">{product.description}</Typography>
-                  {product.image && (
-                    <img
-                      src={renderImage(product.image)}
-                      alt={product.name}
-                      style={{ maxWidth: "100%", height: "auto" }}
-                    />
-                  )}
+                  <img
+                    src={"/api/product/image/" + product._id}
+                    alt={product.name}
+                    style={{ maxWidth: "100%", height: "auto" }}
+                  />
                 </CardContent>
               </Card>
             </Grid>
