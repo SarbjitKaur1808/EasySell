924ab57ea7b14284c94c8e52aa17a2a63604658d
GithubDamini <dbhoi2@my.centennialcollege.ca>
2023-12-04 21:33:42 -0500

    Search with dynamic Category, clickable products

--- client/core/Home.jsx
+++ client/core/Home.jsx
@@ -1,16 +1,18 @@
-import React, { useState, useEffect } from "react";
-import { Card, CardContent, Typography, Grid, Box } from "@material-ui/core";
+import React, { useState, useEffect } from 'react';
+import { Card, CardContent, Typography, Grid, Box } from '@material-ui/core';
+import { Link } from 'react-router-dom'; // Import Link
 import Search from '../product/Search';
-import { list } from "../product/api-product";
+import { list, listCategories } from '../product/api-product'; // Import listCategories
 
 export default function Home() {
   const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]); // New state for categories
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
+  const [error, setError] = useState('');
 
   const loadProducts = (searchQuery = '', category = '') => {
     setLoading(true);
-    list({search: searchQuery, category: category})
+    list({ search: searchQuery, category: category })
       .then(data => {
         if (data.error) {
           setError(data.error);
@@ -21,36 +23,51 @@ export default function Home() {
       });
   };
 
+  // Load categories from the API
+  useEffect(() => {
+    setLoading(true);
+    listCategories().then(data => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setCategories(data);
+      }
+      setLoading(false);
+    });
+  }, []);
+
   useEffect(() => {
     loadProducts();
   }, []);
 
   return (
     <div>
-      <Typography variant="h3" component="h1" gutterBottom>
+      <Typography variant='h3' component='h1' gutterBottom>
         Welcome
       </Typography>
       <Box mb={4}>
-        <Search onSearch={loadProducts} />
+        <Search categories={categories} onSearch={loadProducts} /> {/* Pass categories to Search */}
       </Box>
       {loading && <Typography>Loading products...</Typography>}
-      {error && <Typography color="error">{error}</Typography>}
+      {error && <Typography color='error'>{error}</Typography>}
       <Box mt={4}>
         {!loading && products.length > 0 && (
           <Grid container spacing={3}>
-            {products.map((product) => (
+            {products.map(product => (
               <Grid item xs={12} sm={6} md={3} lg={2} key={product._id}>
-                <Card style={{ maxWidth: "300px", height: "100%" }}>
-                  <CardContent>
-                    <Typography variant="h6">{product.name}</Typography>
-                    <Typography variant="body2">{product.description}</Typography>
-                    <img
-                      src={"/api/product/image/" + product._id}
-                      alt={product.name}
-                      style={{ width: "100%", height: "200px", objectFit: "cover" }}
-                    />
-                  </CardContent>
-                </Card>
+                <Link to={`/product/${product._id}`} style={{ textDecoration: 'none' }}>
+                  <Card style={{ maxWidth: '300px', height: '100%' }}>
+                    <CardContent>
+                      <Typography variant='h6'>{product.name}</Typography>
+                      <Typography variant='body2'>{product.description}</Typography>
+                      <img
+                        src={`/api/product/image/${product._id}`}
+                        alt={product.name}
+                        style={{ width: '100%', height: '200px', objectFit: 'cover' }}
+                      />
+                    </CardContent>
+                  </Card>
+                </Link>
               </Grid>
             ))}
           </Grid>
--- client/product/Search.jsx
+++ client/product/Search.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
@@ -6,38 +6,54 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import SearchIcon from '@material-ui/icons/Search';
 import MenuItem from '@material-ui/core/MenuItem';
+import { listCategories } from './api-product'; // Import the listCategories API call
 
 const useStyles = makeStyles(theme => ({
   card: {
     margin: 'auto',
     textAlign: 'center',
     paddingTop: 10,
-    backgroundColor: '#80808024'
+    backgroundColor: '#80808024',
+    paddingBottom: theme.spacing(2),
   },
-  menu: {
-    width: 200,
+  selectField: {
+    width: 240, // Set a fixed width for the category select field
+    marginRight: theme.spacing(1), // Add some space between the fields
   },
   textField: {
-    marginLeft: theme.spacing(1),
-    marginRight: theme.spacing(1),
-    width: 300,
-    marginBottom: '20px'
+    width: 240, // Set a fixed width for the search text field
   },
   searchButton: {
     minWidth: '20px',
-    height: '30px',
+    height: '55px', // Adjust the height to match TextField height
     padding: '0 8px',
-    marginBottom: '20px'
+    marginTop: '16px', // Align button with TextField
+  },
+  menu: {
+    width: 200, // Ensure the dropdown is also of a fixed width
   }
 }));
+
+// ... (rest of the imports)
 
 export default function Search(props) {
   const classes = useStyles();
   const [search, setSearch] = useState('');
-  const [category, setCategory] = useState('');
+  const [category, setCategory] = useState('All'); // Default to 'All'
+  const [categories, setCategories] = useState(['All']); // Start with 'All' in the array
+
+  useEffect(() => {
+    listCategories().then(data => {
+      if (data && !data.error) {
+        setCategories(['All', ...data]); // Prepend 'All' to the categories from the API
+      } else {
+        console.log(data.error);
+      }
+    });
+  }, []);
 
   const handleSearch = () => {
-    props.onSearch(search, category);
+    props.onSearch(search, category === 'All' ? '' : category); // Pass an empty string for 'All' category
   };
 
   return (
@@ -46,7 +62,7 @@ export default function Search(props) {
         id="select-category"
         select
         label="Select category"
-        className={classes.textField}
+        className={classes.selectField}
         value={category}
         onChange={e => setCategory(e.target.value)}
         SelectProps={{
@@ -56,13 +72,12 @@ export default function Search(props) {
         }}
         margin="normal"
       >
-        <MenuItem value="All">All</MenuItem>
-        <MenuItem value="Electronics">Electronics</MenuItem>
-        <MenuItem value="Books">Books</MenuItem>
-        <MenuItem value="Food">Food</MenuItem>
-        <MenuItem value="Clothing">Clothing</MenuItem>
-        <MenuItem value="Home Decor">Home Decor</MenuItem>
-        {/* Add more categories here */}
+        {/* Include the 'All' option and map through the rest of the categories */}
+        {categories.map((option, index) => (
+          <MenuItem key={index} value={option}>
+            {option}
+          </MenuItem>
+        ))}
       </TextField>
       <TextField
         id="search"
@@ -74,7 +89,7 @@ export default function Search(props) {
         margin="normal"
       />
       <Button variant="contained" color={'primary'} className={classes.searchButton} onClick={handleSearch}>
-        <SearchIcon/>
+        <SearchIcon />
       </Button>
     </Card>
   );
