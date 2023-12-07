74a04f78066c0641cdd8728ec6e027fcb9989371
Vivek Kundaliya <vkundali@my.centennialcollege.ca>
2023-12-07 00:39:08 -0500

    Added index

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use('/api', orderRoutes);
 app.use(express.static(path.join(__dirname, 'client/dist')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+    res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
   });
   
 app.get("/", (req, res) => {
